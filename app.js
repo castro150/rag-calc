cd3b1a60e05cb09456331a1ff3fe1d6f6fac7df8
Rafael Carneiro de Castro <rafaelcarneiroget@hotmail.com>
2017-11-16 08:44:56 -0200

    End of calcs

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict'
 
+let baseLevel, str, dex, luk, weaponLevel, baseWeaponDamage, refinament;
+let bonus, penalty, extraAtk, masteryAtk, buffAtk, aspd;
+
 let readInput = function(id, def = '1') {
   return document.getElementById(id).value || def;
 };
@@ -12,43 +15,51 @@ let readRadioInput = function(name) {
   return checked[0].value;
 };
 
+let readValues = function() {
+  baseLevel = parseFloat(readInput('blvl'));
+  str = parseFloat(readInput('str'));
+  dex = parseFloat(readInput('dex'));
+  luk = parseFloat(readInput('luk'));
+
+  weaponLevel = parseFloat(readInput('wlvl'));
+  baseWeaponDamage = parseFloat(readInput('batk'));
+  refinament = parseFloat(readInput('ref'));
+  bonus = parseFloat(readInput('bonus', '0')) / 100;
+  penalty = parseFloat(readInput('penalty', '100')) / 100;
+
+  extraAtk = parseFloat(readInput('extra', '0'));
+  masteryAtk = parseFloat(readInput('mastery', '0'));
+  buffAtk = parseFloat(readInput('buff', '0'));
+
+  aspd = parseFloat(readInput('aspd'));
+};
+
 let calculate = function() {
-  let mainStat = parseFloat(readInput('str'));
-  let otherStat = parseFloat(readInput('dex'));
+  readValues();
+
+  let mainStat = str;
+  let otherStat = dex;
   if (readRadioInput('wtype') === 'dex') {
-    mainStat = parseFloat(readInput('dex'));
-    otherStat = parseFloat(readInput('str'));
+    mainStat = dex;
+    otherStat = str;
   }
 
   let statusAtk = calculateStatusAtk(mainStat, otherStat);
   let weaponAtk = calculateWeaponAtk(mainStat);
-  let extraAtk = parseFloat(readInput('extra', '0'));
-  let masteryAtk = parseFloat(readInput('mastery', '0'));
-  let buffAtk = parseFloat(readInput('buff', '0'));
 
   let resultMinus = statusAtk + weaponAtk.weaponAtkMinus + extraAtk + masteryAtk + buffAtk;
   let resultPlus = statusAtk + weaponAtk.weaponAtkPlus + extraAtk + masteryAtk + buffAtk;
 
-  let aspd = parseFloat(readInput('aspd'));
   let dps = calculateAtkSec(aspd) * (resultMinus + resultPlus) / 2;
 
   writeResult(statusAtk, weaponAtk, resultMinus, resultPlus, dps);
 };
 
 let calculateStatusAtk = function(mainStat, otherStat) {
-  let baseLevel = parseFloat(readInput('blvl'));
-  let luk = parseFloat(readInput('luk'));
-
   return Math.floor((baseLevel / 4) + mainStat + (otherStat / 5) + (luk / 3));
 };
 
 let calculateWeaponAtk = function(mainStat) {
-  let weaponLevel = parseFloat(readInput('wlvl'));
-  let baseWeaponDamage = parseFloat(readInput('batk'));
-  let refinament = parseFloat(readInput('ref'));
-  let bonus = parseFloat(readInput('bonus', '0')) / 100;
-  let penalty = parseFloat(readInput('penalty', '100')) / 100;
-
   let variance = 0.05 * weaponLevel * baseWeaponDamage;
   let statBonus = baseWeaponDamage * mainStat / 200;
 
